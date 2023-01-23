9e13122b07e201a0eeea454357fcd83ce274290e
Anamaria210 <112964121+Anamaria210@users.noreply.github.com>
2023-01-23 17:04:55 +0000

    day42:Exceptions

--- src/day41_Exceptions/FinallyBlock.java
+++ src/day41_Exceptions/FinallyBlock.java
@@ -1,4 +1,18 @@
 package day41_Exceptions;
 
 public class FinallyBlock {
+    public static void main(String[] args) {
+         int [] arr={1,2,3};
+         try{
+
+             System.out.println(arr[10]);
+             System.out.println("Try Block");
+         }catch (RuntimeException e){
+             System.out.println("Catch Block");
+             e.printStackTrace();
+              System.exit(0); // only way to don't execute the program on finally block
+         }finally {
+             System.out.println("Finally Block");
+         }
+    }
 }
